a44e899436e1ccdcb5614f4083873849ae41f642
jungtop <tenzintopjor27@gmail.com>
2022-01-23 20:01:14 +0530

    openpecha created

--- scrap.py
+++ scrap.py
@@ -4,7 +4,11 @@ from fileinput import filename
 from unicodedata import name
 from urllib import response
 from bs4 import BeautifulSoup
+from openpecha.core.pecha import OpenPechaFS
+from openpecha.core.layer import InitialCreationEnum, Layer, LayerEnum,PechaMetaData
+from datetime import datetime
 import requests
+
 
 start_url = 'http://gretil.sub.uni-goettingen.de/gretil.html'
 pre_url = 'http://gretil.sub.uni-goettingen.de/'
@@ -37,28 +41,39 @@ def get_links(outlines):
 def parse_tei(link):
     xml = make_request(pre_url+link)
     result = BeautifulSoup(xml,'xml')
-    body = result.find('body')
-    
+    body = result.find('body') 
     divs = body.find_all("div", recursive=False)
     title=result.find('title').text
+    base_dic={}
     if divs:
         if len(divs) == 1:
             divs = divs[0].find_all('div',recursive=False)
             
         for div in divs:
-            filename = div.find("head").text
-            with open(f"{filename}.txt","w") as f:
-                f.write(div.get_text().replace(filename,'').strip("\n"))
+            filename = title if div.find("head") == None else div.find("head").text
+            base_dic.update({filename:div.get_text().replace(filename,'').strip("\n")})         
     else:
-        base_text = body.get_text().strip("\n")
-        filename = title
-        """ texts = body.find_all('p',recursive=False)
-        for text in texts:
-            base_text+=text.text+"\n" """
-            
-        with open(f"{filename}.txt","w") as f:
-            f.write(base_text)
+        base_dic.update({title: body.get_text().strip("\n")})
+
+    opf_path = create_opf(base_dic)
+
+def create_opf(base_text):
+    opf_path="./opfs"
+    instance_meta = PechaMetaData(
+        initial_creation_type=InitialCreationEnum.input,
+        created_at=datetime.now(),
+        last_modified_at=datetime.now(),
+        source_metadata="")
+
+    opf = OpenPechaFS(
+        meta=instance_meta,
+        base=base_text
+        )
+
+    opf_path = opf.save(output_path=opf_path)
+    return opf_path
+
 
 if __name__ == '__main__':
-    #parse_page(start_url)
-    parse_tei('gretil/corpustei/sa_sAmavedasaMhitA.xml')
+    parse_page(start_url)
+    #parse_tei('gretil/corpustei/sa_Rgveda-edAufrecht.xml')
