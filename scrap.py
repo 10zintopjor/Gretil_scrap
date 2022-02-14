02072b2b4f44a9b9b11ce88f4bd1e1d721cdf9f4
jungtop <tenzintopjor27@gmail.com>
2022-02-14 15:20:36 +0530

    Added readme and publish

--- scrap.py
+++ scrap.py
@@ -1,23 +1,19 @@
-from curses import flash
-from email.mime import base
-from fileinput import filename
-from unicodedata import name
-from unittest import result
-from urllib import response
 from bs4 import BeautifulSoup
 from openpecha.core.pecha import OpenPechaFS
 from openpecha.core.layer import InitialCreationEnum, Layer, LayerEnum,PechaMetaData
 from datetime import datetime
+from openpecha import github_utils,config
+from pathlib import Path
 import requests
+import re
 
 
 start_url = 'http://gretil.sub.uni-goettingen.de/gretil.html'
 pre_url = 'http://gretil.sub.uni-goettingen.de/'
 
+
 def make_request(url):
-
     response = requests.get(url)
-
     return response
 
 
@@ -32,13 +28,13 @@ def parse_links(outlines):
     for outline in outlines:
         ol = outline.find('ol',attrs={'class':'org-ol'})
         lis = ol.find_all('li',recursive=False)
-
         for li in lis:
             link = li.find('a',text = 'TEI-conformant XML')
             if link:
                 parse_tei(link['href'])
+                print("DONE")
+                return
                 
-
 
 def parse_tei(link):
     xml = make_request(pre_url+link)
@@ -50,28 +46,33 @@ def parse_tei(link):
         base_text={}
         if divs and divs[0].find_all('div',recursive=False) == None:
             if len(divs) == 1:
-                divs = divs[0].find_all('div',recursive=False) 
-
-                
+                divs = divs[0].find_all('div',recursive=False)               
             for div in divs:
                 filename = title if div.find("head") == None else div.find("head").text
                 base_text.update({filename:div.get_text().replace(filename,'').strip("\n")})         
         else:
             base_text.update({title: body.get_text().strip("\n")})
+        #base_text=extract_text_attr(base_text[title])
+        opf_path,src_meta = create_opf(base_text,result)
+        opf_path = Path(opf_path)
+        create_readme(opf_path,src_meta)
+        publish_pecha(opf_path.parent)
 
-        print(title)
-        opf_path = create_opf(base_text,result)
+
+def extract_text_attr(base_text):
+    text = re.split("\d*",base_text)
+    return text
 
 
 def create_opf(base_text,result):
-    opf_path="./opfs"
-    src_meta =get_metadata(result)
+    opf_path=f"{config.PECHAS_PATH}/"
+    meta,src_meta =get_metadata(result)
     opf = OpenPechaFS(
-        meta=src_meta,
+        meta=meta,
         base=base_text
         )
     opf_path = opf.save(output_path=opf_path)
-    return opf_path
+    return opf_path,src_meta
 
 
 def get_metadata(result):
@@ -81,8 +82,7 @@ def get_metadata(result):
         created_at=datetime.now(),
         last_modified_at=datetime.now(),
         source_metadata=src_meta)
-
-    return instance_meta    
+    return instance_meta,src_meta    
 
 
 def parse_src_meta(result):
@@ -90,17 +90,34 @@ def parse_src_meta(result):
     title_stmt = result.find('titleStmt')
     profileDesc = result.find('profileDesc')
     respStmt = title_stmt.find_all('respStmt')
-
     for elem in respStmt:
         src_meta.update({elem.find('resp').text:elem.find('name').text})
-
     languages = profileDesc.find_all('language')
     src_meta.update({'title':title_stmt.find("title").text})
-    src_meta.update({'lang':[x.text for x in languages]})
+    src_meta.update({'language':[x.text for x in languages]})
     src_meta.update({'term':profileDesc.find('term').text})
-
     return src_meta
-    
+
+
+def create_readme(opf_path,src_meta):
+    readme_path = opf_path.parent / 'readme.md'
+    pecha_id = opf_path.stem
+    pecha = f"|Pecha id | {pecha_id}"
+    Table = "| --- | --- "
+    Title = f"|Title | {src_meta['title']} "
+    lang = f"|Language | {src_meta['language']}"
+    readme = f"{pecha}\n{Table}\n{Title}\n{lang}"
+    Path(readme_path).touch()
+    Path(readme_path).write_text(readme)
+
+
+def publish_pecha(opf_path):
+    github_utils.github_publish(
+    opf_path,
+    not_includes=[],
+    message="initial commit"
+    )
+
 
 if __name__ == '__main__':
     parse_page(start_url)
